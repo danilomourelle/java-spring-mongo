0da4c4759b49e8b160b52d62e21149c83e9fccc7
Danilo Mourelle <danilo.mourelle@icarotech.com>
2024-07-23 18:00:07 -0300

    simple Query repo

--- src/main/java/com/danmou/course/repository/PostRepo.java
+++ src/main/java/com/danmou/course/repository/PostRepo.java
@@ -3,6 +3,7 @@ package com.danmou.course.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.danmou.course.domain.Post;
@@ -10,4 +11,7 @@ import com.danmou.course.domain.Post;
 @Repository
 public interface PostRepo extends MongoRepository<Post, String> {
   List<Post> findByTitleContainingIgnoreCase(String text);
+
+  @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+  List<Post> findByTitle(String text);
 }
