d2afca0b60bc33448d8e8d844eb823b7c86797a6
Danilo Mourelle <danilo.mourelle@icarotech.com>
2024-07-23 17:30:03 -0300

    Post by id

---
+++ src/main/java/com/danmou/course/resources/PostResource.java
@@ -0,0 +1,26 @@
+package com.danmou.course.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.danmou.course.domain.Post;
+import com.danmou.course.services.PostService;
+
+@RestController
+@RequestMapping(value = "/post")
+public class PostResource {
+
+  @Autowired
+  private PostService service;
+
+  @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+  public ResponseEntity<Post> findById(@PathVariable String id) {
+    Post post = service.findById(id);
+
+    return ResponseEntity.ok().body(post);
+  }
+}
---
+++ src/main/java/com/danmou/course/services/PostService.java
@@ -0,0 +1,28 @@
+package com.danmou.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.danmou.course.domain.Post;
+import com.danmou.course.repository.PostRepo;
+import com.danmou.course.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+  @Autowired
+  private PostRepo repository;
+
+  public List<Post> findAll() {
+    return repository.findAll();
+  }
+
+  public Post findById(String id) {
+    Optional<Post> Post = repository.findById(id);
+
+    return Post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+  }
+}
